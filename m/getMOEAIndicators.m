d0586b080cc193c7ca418987c87c922be72cbb8a
noz <nh295@cornell>
2016-03-31 18:07:50 -0400

    finished running random select for SSIBEA

--- m/getMOEAIndicators.m
+++ m/getMOEAIndicators.m
@@ -7,43 +7,48 @@ function [fHV,IGD,finalHV,finalIGD,ET] = getMOEAIndicators(filename,npts)
 %HV is the hypervolume
 %IGD is the inverted generational distance
 
-% data = csvread(filename,0,1);
-fid = fopen(filename,'r');
-IGD = zeros(1,npts);
-fHV = zeros(1,npts);
-ET = zeros(1,npts);
-while(~feof(fid))
-    line = strsplit(fgetl(fid),',');
-    switch line{1}
-        case{'InvertedGenerationalDistance'}
-            tIGD = readLine(line);
-%         case{'Number of Injections'}
-%             Inj = readLine(line);
-%         case{'NFE'}
-        case{'Elapsed Time'}
-            tET = readLine(line);
-        case{'FastHypervolume'}
-            tfHV = readLine(line);
-        case{'Final HV'}
-            finalHV = readLine(line);
-        case{'Final IGD'}
-            finalIGD = readLine(line);
-        otherwise
-            continue;
+try
+    % data = csvread(filename,0,1);
+    fid = fopen(filename,'r');
+    IGD = zeros(1,npts);
+    fHV = zeros(1,npts);
+    ET = zeros(1,npts);
+    while(~feof(fid))
+        line = strsplit(fgetl(fid),',');
+        switch line{1}
+            case{'InvertedGenerationalDistance'}
+                tIGD = readLine(line);
+                %         case{'Number of Injections'}
+                %             Inj = readLine(line);
+                %         case{'NFE'}
+            case{'Elapsed Time'}
+                tET = readLine(line);
+            case{'FastHypervolume'}
+                tfHV = readLine(line);
+            case{'Final HV'}
+                finalHV = readLine(line);
+            case{'Final IGD'}
+                finalIGD = readLine(line);
+            otherwise
+                continue;
+        end
     end
+    fclose(fid);
+    len = length(tIGD);
+    %get end of run indicator values
+    temp = min([len,npts]);
+    IGD(1:temp) = tIGD(1:temp);
+    fHV(1:temp) = tfHV(1:temp);
+    ET(1:temp) = tET(1:temp);
+catch ME
+    warning(strcat('Problem file: ', filename));
+    rethrow(ME);
 end
-fclose(fid);
-len = length(tIGD);
-%get end of run indicator values
-temp = min([len,npts]);
-IGD(1:temp) = tIGD(1:temp);
-fHV(1:temp) = tfHV(1:temp);
-ET(1:temp) = tET(1:temp);
 end
 
 function [out] = readLine(line)
 out = zeros(length(line)-1,1);
 for i=1:length(line)-1
-       out(i)=str2double(line{i+1});
+    out(i)=str2double(line{i+1});
 end
 end
--- m/plot1Opresults.m
+++ m/plot1Opresults.m
@@ -4,16 +4,16 @@ function plot1Opresults
 %(jmetal) and the additive epsilon values for each algorithm
 
 % problemName = {'UF1','UF2','UF3','UF4','UF5','UF6','UF7','UF8','UF9','UF10'};
-% problemName = { 'DTLZ1','DTLZ2','DTLZ3','DTLZ4','DTLZ5','DTLZ6','DTLZ7'};
- problemName = {'WFG1','WFG2','WFG3','WFG4','WFG5','WFG6','WFG7','WFG8','WFG9'};
-% MOEA =  {'MOEAD'};
-MOEA =  {'SSIBEA'};
+problemName = { 'DTLZ1','DTLZ2','DTLZ3','DTLZ4','DTLZ5','DTLZ6','DTLZ7'};
+%  problemName = {'WFG1','WFG2','WFG3','WFG4','WFG5','WFG6','WFG7','WFG8','WFG9'};
+MOEA =  {'MOEAD'};
+% MOEA =  {'SSIBEA'};
 % MOEA =  {'eMOEA','MOEAD'};
 operator = {'sbx+pm','de+pm','um','pcx+pm','undx+pm','spx+pm'};
 operatorName = {'SBX','DE','UM','PCX','UNDX','SPX'};
 
-path = '/Users/nozomihitomi/Dropbox/MOHEA/';
-% path = 'C:\Users\SEAK2\Nozomi\MOHEA\';
+% path = '/Users/nozomihitomi/Dropbox/MOHEA/';
+path = 'C:\Users\SEAK2\Nozomi\MOHEA\';
 % path = 'C:\Users\SEAK1\Dropbox\MOHEA\';
 res_path =strcat(path,'mResExperimentA');
 
